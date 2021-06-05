e93557d98b0dff31763a6a72535f15f54f2b5bbd
raj.ratre <raj.ratre@KCDESK-GR150>
2021-06-05 11:07:15 +0530

    Add project files.

---
+++ DataType/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DataType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Size of int data type",sizeof(int));
+            Console.ReadKey();
+        }
+    }
+}
