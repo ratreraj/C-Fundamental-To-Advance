3d498da2f7e1daa309c9162f3a8d1ee52a0d0296
raj.ratre <raj.ratre@KCDESK-GR150>
2021-06-24 07:58:12 +0530

    Encapsulation

---
+++ Encasulation/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encasulation
+{
+    public class Program
+    {
+        //private double balance;
+
+        //public double GetBalance()
+        //{
+
+        //    return balance;
+        //}
+        //public double SetBalance(double _balnace)
+        //{
+        //    if (_balnace < 0)
+        //    {
+        //        Console.WriteLine(" Negative value is not required");
+
+        //    }
+        //    else
+        //    {
+        //         this.balance = _balnace;
+
+        //    }
+        //    return this.balance;
+        //}
+
+        private double _balance;
+
+
+        public double balance
+        {
+            get
+            {
+
+                return _balance;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine(" Negative value is not required");
+                    value = 0;
+                }
+               
+                _balance = value;
+            }
+        }
+
+
+    }
+    public class Banimp
+    {
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.balance = -10;
+            Console.WriteLine("Balance - {0}", obj.balance);
+            Console.ReadKey();
+        }
+    }
+
+}
--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -12,34 +12,41 @@ namespace Inheritance
         internal int CHid;
         internal string CHname;
         internal string CHaddrsss;
-        public child()
+
+        public child(string value) : base(value)
         {
             Console.WriteLine("Child class constructor calling");
         }
         static void Main(string[] args)
         {
-            child objchild = new child();
-            //objchild.
-            Parrent objp = new Parrent();
-            //objp.
-            Parrent pp = new child();
-            
+            string Value2 = "Value 1";
+            child objchild = new child(Value2);
+            ////objchild.
+            //Parrent objp = new Parrent();
+            ////objp.
+            //Parrent pp = new child();
+
             Console.WriteLine("Hello World!");
             Console.ReadKey();
         }
     }
 
-    class Parrent 
+    class Parrent
     {
         internal int id;
         internal string name;
         internal string addrsss;
 
-        public Parrent()
-        {
-            Console.WriteLine("Parent class constructor calling");
-        }
+        //public Parrent()
+        //{
+        //    Console.WriteLine("Parent class constructor calling");
+        //}
 
+
+        public Parrent(string value1)
+        {
+            Console.WriteLine("Parent class constructor calling - {0}", value1);
+        }
     }
 
     class GrandParent
@@ -47,6 +54,7 @@ namespace Inheritance
         internal int id;
         internal string name;
         internal string addrsss;
+
         public GrandParent()
         {
             Console.WriteLine("GrandParent class constructor calling");
