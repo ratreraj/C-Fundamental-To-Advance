a16b90ce9d62de3ca15f2e85ec9c997fb943293f
raj.ratre <raj.ratre@KCDESK-GR150>
2021-06-28 07:49:18 +0530

    Multiple Inheritance and Genric Delegates

---
+++ Delegates/GenricsDelegates.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    //public delegate double delAddResult(int num1, float num2, double num3);
+    //public delegate void delGetResult(int num1);
+    //public delegate bool delResult(bool flag);
+
+    //---------------------------------------
+
+    class GenricsDelegates
+    {
+        public double AddResult(int num1, float num2, double num3)
+        {
+            return num1 + num2 + num3;
+        }
+
+        public void GetResult(int num1)
+        {
+            Console.WriteLine("Result - {0}", num1);
+        }
+
+        public bool Result(bool flag)
+        {
+            return flag;
+        }
+
+        static void Main(string[] args)
+        {
+            //GenricsDelegates delegates = new GenricsDelegates();
+            //// First Delegates
+            //delAddResult delAdd = new delAddResult(delegates.AddResult);
+            //double res = delAdd.Invoke(10, 20.25f, 50.52f);
+            //Console.WriteLine("Result - {0}", res);
+            //// Second Delegates 
+            //delGetResult delGet = new delGetResult(delegates.GetResult);
+            //delGet.Invoke(10);
+
+            //// Third Delegates
+            //delResult del = new delResult(delegates.Result);
+            //del.Invoke(true);
+
+
+            // -------------- Anonymous method 
+
+            //GenricsDelegates delegates = new GenricsDelegates();
+            //// Func Delegates is used for only value retuning methods
+            //Func<int,float,double,double> delAdd =  (delegates.AddResult);
+            //double res = delAdd.Invoke(10, 20.25f, 50.52f);
+            //Console.WriteLine("Result - {0}", res);
+            //// Action delegates are used for void type method  
+            //Action<int> delGet =  (delegates.GetResult);
+            //delGet.Invoke(10);
+
+            //// Predicate Delegates is only for bool return method
+            //Predicate<bool> del =  (delegates.Result);
+            //del.Invoke(true);
+
+
+
+            // -------------- lambda expression method 
+
+            //GenricsDelegates delegates = new GenricsDelegates();
+            // Func Delegates is used for only value retuning methods
+            Func<int, float, double, double> delAdd = (x, y, z) => x + y + z;
+            double res = delAdd.Invoke(10, 20.25f, 50.52f);
+            Console.WriteLine("Result - {0}", res);
+            // Action delegates are used for void type method  
+            Action<int> delGet = (x) => Console.WriteLine("Result - {0}", x);
+            delGet.Invoke(10);
+
+            // Predicate Delegates is only for bool return method
+            Predicate<bool> del = (f) => f;
+            del.Invoke(true);
+
+        }
+    }
+
+
+}
---
+++ Inheritance/MultipleInheritance.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+
+    public interface ITest1
+    {
+        void Test();
+        void Show();
+    }
+
+    public interface ITest2
+    {
+        void Test();
+        void Show2();
+    }
+
+    public class HybridTest
+    {
+        public void Show3()
+        {
+
+            Console.WriteLine(" Show method is calling from HybridTest class");
+        }
+    }
+
+    class InterfaceImplemention : HybridTest, ITest1, ITest2
+    {
+        void ITest1.Show()
+        {
+            Console.WriteLine(" Show method is calling from Itest1");
+        }
+
+        void ITest1.Test()
+        {
+            Console.WriteLine(" Print method is calling from Itest1");
+        }
+
+        public void Show2()
+        {
+            Console.WriteLine(" Show method is calling from Itest2");
+        }
+
+        void ITest2.Test()
+        {
+            Console.WriteLine(" Print method is calling from Itest2");
+        }
+    }
+    class MultipleInheritance
+    {
+        static void Main(string[] args)
+        {
+            InterfaceImplemention @interface = new InterfaceImplemention();
+
+            //@interface.Test();
+            //@interface.Show2();
+            @interface.Show3();
+
+            ITest1 test1 = @interface;
+            test1.Test();
+            test1.Show();
+           
+
+            ITest2 test2 = @interface;
+            test2.Test();
+            test2.Show2();
+
+            Console.ReadKey();
+
+        }
+    }
+}
