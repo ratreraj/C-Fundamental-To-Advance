ceca6dafc3547f413344dea7a17e7dad285f6a23
raj.ratre <raj.ratre@KCDESK-GR150>
2021-07-01 07:59:48 +0530

    Abstract Class

---
+++ AbstractClass/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+
+    interface demo
+    {
+
+        void print();
+    }
+
+    public  abstract  class Person :demo
+    {
+
+        public string name;
+        public static string fullname;
+        public  void SayHello(string str)
+        {
+            Console.WriteLine(" Hello my name is {0}", str);
+        }
+
+
+        public abstract  string FullName(string fname, string mname, string lname);
+
+        public abstract void print();
+       
+    }
+}
+
+
---
+++ AbstractClass/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Program : Person
+    {
+        public override string FullName(string fname, string mname, string lname)
+        {
+            return fname + " " + mname + " " + lname;
+        }
+
+        public override void print()
+        {
+            throw new NotImplementedException();
+        }
+        static void Main(string[] args)
+        {
+            Program person = new Program();
+            Person persons = new Program();
+           
+            string fullname = person.FullName("Raj", "Prakash", "Ratre");
+            person.SayHello("Raj Parkash Ratre");
+            Console.WriteLine(fullname);
+            Console.ReadKey();
+        }
+
+      
+    }
+}
