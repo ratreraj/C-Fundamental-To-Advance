83de55616814d058909108ad7d98b1e165d77e50
Raj Prakash Ratre <8319633+ratreraj@users.noreply.github.com>
2021-08-11 08:08:38 +0530

    Genrics

---
+++ Collections/ArraysList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Collections
+{
+    public class ArraysList
+    {
+
+        //string [] vs = [10];
+        //string[] vs =  { "", "" };
+
+        
+
+    }
+}
---
+++ Collections/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] args1 = { 1, "" };
+
+            ArrayList arrayList = new ArrayList();
+            Console.WriteLine(arrayList.Capacity);
+            arrayList.Add("Raj");
+            arrayList.Add(10);
+            arrayList.Add(true);
+            arrayList.Add(10.23f);
+            arrayList.Add(90000);
+            foreach (var item in arrayList)
+            { 
+                 Console.WriteLine(item);
+            
+            }
+            arrayList.RemoveAt(0);
+            arrayList.Insert(2, "Raj");
+            Console.WriteLine("------------------------------\n");
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine(item);
+
+            }
+            Console.WriteLine(arrayList.Capacity);
+
+            Console.ReadKey();
+        }
+    }
+}
